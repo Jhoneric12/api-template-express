b0e8706dea7b290901dd32341831c0e18630748d
unknown <jhonerix23@gmail.com>
2026-04-27 22:25:41 +0800

    fix: apierror

--- server/src/utils/app-error.ts
+++ server/src/utils/app-error.ts
@@ -10,6 +10,6 @@ export class APIError extends Error {
     this.status = statusCode >= 400 && statusCode < 500 ? "fail" : "error";
     this.isOperational = true;
 
-    Error.captureStackTrace(this, this.constructor);
+    (Error as any).captureStackTrace(this, this.constructor);
   }
 }
